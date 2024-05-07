2454dcd222921a7c9efb267dfda3ddf3bb57653b
rokla105 <roman.klamandt@hhu.de>
2024-05-07 12:13:45 +0200

    docker-compose.dev.yml added

--- config.py
+++ config.py
@@ -1,17 +1,13 @@
 import os
-from collections import OrderedDict
-from pathlib import Path
 
 
 class Config(object):
-
     # CONFIGURATIONS
 
     # DB variables
     SQLALCHEMY_DATABASE_URI = "sqlite:///app.database"
 
-
     # APP variables
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
     TEMPLATE_DIR = "app/templates"
-
+    DEBUG = True if os.environ.get("FLASK_ENV") == "development" else False
